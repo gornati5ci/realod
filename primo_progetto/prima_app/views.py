25de14e71aa53dc8d0ed045050a25c440f4f945c
Gornati5Ci <giangorn12345678@gmail.com>
2022-03-07 22:20:15 +0100

    Modifica main

--- primo_progetto/prima_app/views.py
+++ primo_progetto/prima_app/views.py
@@ -20,6 +20,7 @@ def update(request):
     stored on PythonAnywhere in the git.Repo() as parameter.
     Here the name of my directory is "test.pythonanywhere.com"
     '''
+    
     repo = git.Repo("realod/") 
     origin = repo.remotes.origin
     origin.pull()
