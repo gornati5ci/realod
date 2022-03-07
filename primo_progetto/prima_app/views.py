f4d51f01a587ce3b88114f6f02772b3a1f014d15
Gornati5Ci <giangorn12345678@gmail.com>
2022-03-07 22:29:34 +0100

    Test reload

--- primo_progetto/prima_app/views.py
+++ primo_progetto/prima_app/views.py
@@ -9,12 +9,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+import requests
+
+
 @csrf_exempt
 def update(request):
   if request.method == "POST":
-    print(request.POST)
-    if request.POST.ref!="refs/heads/main":
-      return HttpResponse("Invalid branch. Update aborted")
     '''
     pass the path of the directory where your project will be 
     stored on PythonAnywhere in the git.Repo() as parameter.
@@ -24,7 +24,21 @@ def update(request):
     repo = git.Repo("realod/") 
     origin = repo.remotes.origin
     origin.pull()
+    
+    username = "Commentatore"
+    api_token = "96adb770cf3be1028f4e517c8222d80e5378ef2f"
+    domain_name = "Commentatore.pythonanywhere.com"
 
-    return HttpResponse("Updated code on PythonAnywhere!")
+    response = requests.post(
+      'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
+        username=username, domain_name=domain_name
+      ),
+      headers={'Authorization': 'Token {token}'.format(token=api_token)}
+    )
+    if response.status_code == 200:
+      return HttpResponse("Updated code on PythonAnywhere!")
+    else:
+      return HttpResponse('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
   else:
     return HttpResponse("Couldn't update the code on PythonAnywhere")
