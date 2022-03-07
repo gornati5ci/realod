0d5a47ca0edf04ef73b429c51832fea6ebde8bf6
Gornati5Ci <giangorn12345678@gmail.com>
2022-03-07 22:19:02 +0100

    Funziona?

--- primo_progetto/prima_app/views.py
+++ primo_progetto/prima_app/views.py
@@ -25,6 +25,6 @@ def update(request):
 
     origin.pull()
 
-    return HttpResponse("Updated code on PythonAnywhere")
+    return HttpResponse("Updated code on PythonAnywhere!")
   else:
     return HttpResponse("Couldn't update the code on PythonAnywhere")
